dd40aad57682aa7b55015ba9913e1b05102ce9a6
sebdok <seb.dok@gmail.com>
2018-09-09 10:49:01 +0200

    koniguracja propertis

--- src/main/java/com/sda/hibernate/Main.java
+++ src/main/java/com/sda/hibernate/Main.java
@@ -1,4 +1,23 @@
 package com.sda.hibernate;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
+
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+            sessionFactory = configuration.buildSessionFactory();
+        }catch (Throwable ex){
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
