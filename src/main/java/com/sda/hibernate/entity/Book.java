ab76b8f795869b806b1dae765bf0144b40929ce3
sebdok <seb.dok@gmail.com>
2018-09-09 11:40:12 +0200

    dodajemy now klase 'Book' i robimy 'maping' w propertis

---
+++ src/main/java/com/sda/hibernate/entity/Book.java
@@ -0,0 +1,26 @@
+package com.sda.hibernate.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    @Column(name = "")
+    private String title;
+    @Column(name = "")
+    private String isbn;
+    @Column(name = "")
+    private String author;
+
+    public Book(String title, String isbn, String author) {
+        this.title = title;
+        this.isbn = isbn;
+        this.author = author;
+    }
+}
